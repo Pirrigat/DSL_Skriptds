ed14fdae73875bdae163aa4a9e5c32096f89b071
Pirrigat <pirrigate.team@gmail.com>
2017-07-05 15:07:59 +0200

    Update ModelBusexplorer_DiffMerge_Git_2_0.groovy

--- ModelBusexplorer_DiffMerge_Git_2_0.groovy
+++ ModelBusexplorer_DiffMerge_Git_2_0.groovy
@@ -6,7 +6,7 @@ job('ModelBusExplorer-DiffMerge-Git-2.0') {
             mavenInstallation("Maven")
             goals("clean package pmd:pmd checkstyle:checkstyle")
             properties(skipTest: true)
-		rootPOM("${WORKSPACE}/ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.master/pom.xml")
+		rootPOM("${WORKSPACE}/TychoBuild/ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.master/pom.xml")
         }
                  
     }
@@ -15,7 +15,7 @@ job('ModelBusExplorer-DiffMerge-Git-2.0') {
     		serverUrl("http://10.147.66.165:8080")
     		channel("TestJenkins")
     		deployKey("5c9a6cb4bfd9a006cd1987d90476f39df904a9c40fe1cc533d4416bab51ba048")
-		artifacts("${WORKSPACE}/ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.category/target/*zip")
+		artifacts("${WORKSPACE}/TychoBuild/ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.category/target/*zip")
 		failsAsUpload(true)
 
         }
