90d3d589c3bd1def7a1980b11e96a8b122163e2f
Pirrigat <pirrigate.team@gmail.com>
2017-07-05 17:21:48 +0200

    Add files via upload

---
+++ Modelbus_Server_Product_Git_2_0.groovy
@@ -0,0 +1,19 @@
+job('Modelbus-Server-Product-Git-2.0') {
+    description 'ModelBus Server Build.'
+    customWorkspace('workspace/ModelBus-Core-2.0-Seed/')
+    steps {
+        maven {
+            mavenInstallation("Maven")
+            goals("clean package pmd:pmd checkstyle:checkstyle")
+            properties(skipTest: true)
+			property("platform-version-name", "neon")
+			rootPOM("TychoBuild/modelbus/build/org.modelbus.build.server.product/pom.xml")
+        }
+                 
+    }
+    publishers {
+		archiveArtifacts('TychoBuild\modelbus\build\org.modelbus.build.server.product\target\products\org.modelbus.server.product\*.zip')
+
+        }
+    }
+}
