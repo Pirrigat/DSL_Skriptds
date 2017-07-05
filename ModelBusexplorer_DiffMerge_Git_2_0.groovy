1bc2fbc1b61ff50cd6c7ecf7a108b7d4187f2ba1
Pirrigat <pirrigate.team@gmail.com>
2017-07-05 14:40:14 +0200

    Update ModelBusexplorer_DiffMerge_Git_2_0.groovy

--- ModelBusexplorer_DiffMerge_Git_2_0.groovy
+++ ModelBusexplorer_DiffMerge_Git_2_0.groovy
@@ -1,34 +1,12 @@
 job('ModelBusExplorer-DiffMerge-Git-2.0') {
     description 'ModelBusExplorer DiffMerge Build.'
-    scm {
-        git {
-            remote {
-                name ("origin")
-                url ("git@gitlab.fokus.fraunhofer.de:modelbus/ModelBusExplorer.git")
-                credentials("ModelBus")
-                refspec ("+refs/heads/ecf-version:refs/remotes/origin/ecf-version")
-                extensions{
-                    relativeTargetDirectory("ModelBusExplorer")
-                }
-            }
-            remote {
-                name ("origin1")
-                url ("git@gitlab.fokus.fraunhofer.de:modelbus/DiffMerge.git")
-                credentials("ModelBus")
-                refspec ("+refs/heads/*:refs/remotes/origin1/*")
-                 extensions{
-                    relativeTargetDirectory("DiffMerge")
-                }
-            }
-            branch ("ecf-version")
-        }
-    }
+    customWorkspace('workspace/ModelBus-Core-2.0-Seed/TychoBuild/')
     steps {
         maven {
             mavenInstallation("Maven")
             goals("clean package pmd:pmd checkstyle:checkstyle")
             properties(skipTest: true)
-            rootPOM("ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.master/pom.xml")
+		rootPOM("${WORKSPACE}/ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.master/pom.xml")
         }
                  
     }
@@ -37,8 +15,8 @@ job('ModelBusExplorer-DiffMerge-Git-2.0') {
     		serverUrl("http://10.147.66.165:8080")
     		channel("TestJenkins")
     		deployKey("5c9a6cb4bfd9a006cd1987d90476f39df904a9c40fe1cc533d4416bab51ba048")
-    		artifacts("ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.category/target/*zip")
-			failsAsUpload(true)
+		artifacts("${WORKSPACE}/ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.category/target/*zip")
+		failsAsUpload(true)
 
         }
     }
