4221a33d30932692abf1f1018967c38d48802002
Pirrigat <pirrigate.team@gmail.com>
2017-07-05 15:48:21 +0200

    Update ModelBusexplorer_DiffMerge_Git_2_0.groovy

--- ModelBusexplorer_DiffMerge_Git_2_0.groovy
+++ ModelBusexplorer_DiffMerge_Git_2_0.groovy
@@ -13,7 +13,7 @@ job('ModelBusExplorer-DiffMerge-Git-2.0') {
     publishers {
     	droneRecorder {
     		serverUrl("http://10.147.66.165:8080")
-    		channel("TestJenkins")
+    		channel("Test.ModelbusExplorer.DiffMerge.Git.2.0")
     		deployKey("5c9a6cb4bfd9a006cd1987d90476f39df904a9c40fe1cc533d4416bab51ba048")
 		artifacts("ModelBusExplorer/trunk/org.modelbus.tools.modelbusexplorer.build.category/target/*zip")
 		failsAsUpload(true)
