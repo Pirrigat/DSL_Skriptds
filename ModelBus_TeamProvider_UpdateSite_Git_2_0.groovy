1b9f6e704f87b076ee8c22fd344445dce2717e08
Pirrigat <pirrigate.team@gmail.com>
2017-07-06 10:53:08 +0200

    Update ModelBus_TeamProvider_UpdateSite_Git_2_0.groovy

--- ModelBus_TeamProvider_UpdateSite_Git_2_0.groovy
+++ ModelBus_TeamProvider_UpdateSite_Git_2_0.groovy
@@ -1,20 +1,11 @@
 job('ModelBus-TeamProvider-UpdateSite-Git-2.0') {
     description 'ModelBus TeamProvider-UpdateSite Build.'
-    scm {
-        git {
-            remote {
-                url ("git@gitlab.fokus.fraunhofer.de:modelbus/PapyrusAdapter.git")
-                credentials("ModelBus")
-            }
-            branch ("ecf-version")
-        }
-    }
     steps {
         maven {
             mavenInstallation("Maven")
             goals("clean package")
             properties(skipTest: true)
-            rootPOM("modelbus/build/org.modelbus.build.teamprovider.master/pom.xml")
+            rootPOM("TychoBuild/modelbus/build/org.modelbus.build.teamprovider.master/pom.xml")
         }
                  
     }
@@ -24,7 +15,7 @@ job('ModelBus-TeamProvider-UpdateSite-Git-2.0') {
     		channel("TestJenkins")
     		deployKey("5c9a6cb4bfd9a006cd1987d90476f39df904a9c40fe1cc533d4416bab51ba048")
     		artifacts("modelbus/p2repositories/org.modelbus.build.teamprovider/target/*.zip")
-			failsAsUpload(true)
+		failsAsUpload(true)
 
         }
     }
